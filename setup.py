2b6b16dff6d4045e4a1263392b226b3759823b40
pdamianik <39028343+pdamianik@users.noreply.github.com>
2020-08-09 10:38:00 +0200

    :cd: Added compile profiles to setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,43 @@
 #!/usr/bin/python3
 import setuptools
+import sys
 import PyInstaller.__main__ as pim
 
-pim.run([
-	"--name=DodgeToDrums",
-	"--onefile",
-	"--windowed",
-	"--clean",
-	"--exclude-module=tkinter",
-	"--exclude-module=PyQt4",
-	"--exclude-module=PyQt5",
-	"--exclude-module=numpy",
-	"--hidden-import=packaging.requirements",
-#	"--icon=icon.ico",
-#	"--add-data=./data/",
-	"main.py"
-	])
+DEFAULT_PROFILE = "dist"
+
+if len(sys.argv) > 0:
+	PROFILE = sys.argv[1]
+else:
+	PROFILE = DEFAULT_PROFILE
+
+PROFILES = {
+	"dist": [
+		"--name=DodgeToDrums",
+		"--onefile",
+		"--windowed",
+		"--clean",
+		"--exclude-module=tkinter",
+		"--exclude-module=PyQt4",
+		"--exclude-module=PyQt5",
+		"--exclude-module=numpy",
+		"--hidden-import=packaging.requirements",
+	#	"--icon=icon.ico",
+	#	"--add-data=./data/",
+		"main.py"
+	],
+	"dev": [
+		"--name=DodgeToDrums",
+		"--onefile",
+		"--clean",
+		"--exclude-module=tkinter",
+		"--exclude-module=PyQt4",
+		"--exclude-module=PyQt5",
+		"--exclude-module=numpy",
+		"--hidden-import=packaging.requirements",
+	#	"--icon=icon.ico",
+	#	"--add-data=./data/",
+		"main.py"
+	]
+}
+
+pim.run(PROFILES[PROFILE])
