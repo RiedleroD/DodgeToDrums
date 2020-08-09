35ded824f4ed277a8f1e42ac24196d95f5c97323
pdamianik <39028343+pdamianik@users.noreply.github.com>
2020-08-09 20:50:47 +0200

    :bulb: Added option to print the build arguments for pyinstaller

--- setup.py
+++ setup.py
@@ -25,11 +25,25 @@ PROFILES = {
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Builds DodgeToDrums")
-	parser.add_argument("profile", type=str, nargs='?', help="The build-profile")
+	parser.add_argument(
+		"profile",
+		type=str,
+		nargs='?',
+		help="The build-profile")
+	parser.add_argument(
+		"--print-args",
+		action="store_true",
+		default=False,
+		help="prints the arguments for pyinstaller")
 	args = parser.parse_args()
 
-	if args.__contains__("profile") and args.profile is not None:
-		PROFILE = args.profile
-		pim.run(DEFAULT_PROFILE.symmetric_difference(PROFILES[PROFILE]))
+	if args.__contains__("profile") and args.profile is None:
+		build_arguments = DEFAULT_PROFILE
 	else:
-		pim.run(DEFAULT_PROFILE)
+		PROFILE = args.profile
+		build_arguments = DEFAULT_PROFILE.symmetric_difference(PROFILES[PROFILE])
+
+	if args.__contains__("print_args") and args.print_args:
+		print(" ".join(build_arguments))
+	else:
+		pim.run(build_arguments)
